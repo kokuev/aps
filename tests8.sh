488b4f3defa56f0cf2cf7e88a1561441c351bd88
Alexander Kokuev <alexander.a.kokuev@gmail.com>
2014-06-14 21:13:50 +0400

    no message

--- tests8.sh
+++ tests8.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
 while true
 do
-	for f in `cd tasks && ls task_2x2*`;
+	echo "new iteration"
+	for f in `cd tasks && ls task8*`;
 	do
 		echo $f
 		python3.3 aps.py tasks/$f result/$f.pck >> logs/$f.txt;
